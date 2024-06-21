6b63af004b845ef1ead417d4edff3b8d59aca692
phischa <ph.git@mailbox.org>
2024-06-21 20:18:05 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,
+    RouterOutlet, 
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/main-content/landingPage/landingPage.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <app-navbar></app-navbar>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+    </section>    
+    `,
+    styleUrls: ['./landingPage.component.scss']
+})
+
+export class LandingPageComponent {
+
+}
