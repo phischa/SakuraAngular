6aa2a5a3463b26dcfe440fdebea3c9efeeb20880
phischa <ph.git@mailbox.org>
2024-06-21 22:37:44 +0200

    2. commit

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,7 +6,10 @@ import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, OurMenuComponent, HowToOrderComponent],
+  imports: [LandingPageComponent,
+    OurMenuComponent,
+    HowToOrderComponent
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
